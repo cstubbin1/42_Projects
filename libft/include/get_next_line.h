873258a4c0bcec79f465482736cb555287705264
cstubbin1 <100103359+cstubbin1@users.noreply.github.com>
2022-10-10 10:06:16 +1030

    Add files via upload

---
+++ libft/include/get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cstubbin <cstubbin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/28 09:58:08 by cstubbin          #+#    #+#             */
+/*   Updated: 2022/09/01 11:01:15 by cstubbin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+# define BUFF_SIZE	1024
+
+char	*get_next_line(int fd);
+
+#endif
